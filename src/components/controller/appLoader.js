de93b07ab671f9b10220c40b39cb1c668bb08626
marichemia <marichemia10@gmail.com>
2022-07-31 12:37:11 +0400

    initial commit: inserted api key, created tsconfig

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '4c0a290d45394ed5bff1b0b8b566b98e', // получите свой ключ https://newsapi.org/
         });
     }
 }
